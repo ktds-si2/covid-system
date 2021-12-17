20430a6cf17e42e1170b7253111560289e7c4441
Jang Hyeon Woong <ds6tvi@gmail.com>
2021-12-17 09:25:26 +0900

    (BE) User 컨트롤러 엔드포인트 생성

---
+++ backend/src/main/java/com/ktds/covidsystem/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.ktds.covidsystem.controller;
+
+import com.ktds.covidsystem.dto.SignupRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+//  유저 컨트롤러  Author : JHW
+@RestController
+@Slf4j
+public class UserController {
+
+    // 로그인
+    @PostMapping("/login")
+    public ResponseEntity login() {
+        log.info("POST /login");
+        return null;
+    }
+
+    // 로그아웃
+    @GetMapping("/logout")
+    public ResponseEntity logout() {
+        log.info("GET /logout");
+
+        return null;
+    }
+
+    // 회원가입
+    @PostMapping("/signup")
+    public ResponseEntity signup(@RequestBody SignupRequest signupRequest) {
+        log.info("POST /signup : {}", signupRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+
+}
---
+++ backend/src/main/java/com/ktds/covidsystem/service/UserService.java
@@ -0,0 +1,8 @@
+package com.ktds.covidsystem.service;
+
+import org.springframework.stereotype.Service;
+
+// 유저 서비스  Author : JHW
+@Service
+public class UserService {
+}
