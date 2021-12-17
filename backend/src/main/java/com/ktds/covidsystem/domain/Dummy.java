1884920036bde6abf1ef664fe0e6171c6b6466d1
lavine <43743383+wlgns5@users.noreply.github.com>
2021-12-17 13:30:37 +0900

    Revert "Feature/be add admin controller"

---
+++ backend/src/main/java/com/ktds/covidsystem/domain/Dummy.java
@@ -0,0 +1,4 @@
+package com.ktds.covidsystem.domain;
+
+public class Dummy {
+}
--- backend/src/main/java/com/ktds/covidsystem/service/AdminService.java
+++
@@ -1,8 +0,0 @@
-package com.ktds.covidsystem.service;
-
-import org.springframework.stereotype.Service;
-
-// Author : KJH
-@Service
-public class AdminService {
-}
