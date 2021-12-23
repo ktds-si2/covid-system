aa673e8cb4e0817745d23eab29d237238b7db325
Lavine <zihun35@gmail.com>
2021-12-23 16:14:01 +0900

    (BE) 즐겨찾기 기능 추가

---
+++ backend/src/main/java/com/ktds/covidsystem/repository/FavoriteRepositoryQSL.java
@@ -0,0 +1,13 @@
+package com.ktds.covidsystem.repository;
+
+import com.ktds.covidsystem.constant.PlaceType;
+import com.ktds.covidsystem.dto.FavoriteResponseDto;
+import com.ktds.covidsystem.dto.PlaceDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+// Author : KJH
+public interface FavoriteRepositoryQSL {
+    public Page<FavoriteResponseDto> findFavorite(Pageable pageable);
+
+}
