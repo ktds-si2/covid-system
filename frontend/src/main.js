e96467271c2197ca29143e0bd31d466b1ada5c44
Jang Hyeon Woong <ds6tvi@gmail.com>
2021-12-27 16:47:52 +0900

    (FE) Header 생성 및 기본구조 맞추기

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
