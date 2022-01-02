9bf37702ef7b5ff3b00ff0fb34dbc2d1adc46e4d
Lavine <zihun35@gmail.com>
2022-01-02 22:08:04 +0900

    (FE) drawer, appbar 분리

---
+++ frontend/src/components/layout/appbar.vue
@@ -0,0 +1,43 @@
+<!-- Author : JHW -->
+<template>
+  <v-app-bar color="white" flat app>
+    <v-container class="py-6 fill-height">
+      <v-avatar class="mr-10" color="grey darken-1" size="32"></v-avatar>
+
+      <v-btn v-for="link in links" :key="link" text>
+        {{ link }}
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-responsive max-width="260">
+        <v-text-field
+          dense
+          flat
+          hide-details
+          rounded
+          solo-inverted
+          v-model="data"
+          v-on:keyup.enter="input"
+        ></v-text-field>
+      </v-responsive>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "appbar",
+  data() {
+    return {
+      data: '',
+      links: ["arr1", "arr2", "Profile", "Updates"],
+    };
+  },
+  methods: {
+    input() {
+      // this.$store.dispatch()
+    }
+  }
+};
+</script>
