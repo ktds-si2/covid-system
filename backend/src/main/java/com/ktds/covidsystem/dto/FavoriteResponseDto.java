d83a90f453220e7e6ed250b6952811f740127536
Lavine <zihun35@gmail.com>
2022-01-03 13:40:38 +0900

    (BE) 즐겨찾기 가져오기 기능 로직 변경

--- backend/src/main/java/com/ktds/covidsystem/dto/FavoriteResponseDto.java
+++ backend/src/main/java/com/ktds/covidsystem/dto/FavoriteResponseDto.java
@@ -1,5 +1,7 @@
 package com.ktds.covidsystem.dto;
 
+import com.ktds.covidsystem.domain.Favorite;
+
 // author: KJH
 public record FavoriteResponseDto(
         Long favorite_id,
@@ -17,5 +19,18 @@ public record FavoriteResponseDto(
 
     public FavoriteResponseDto(Long place_id) {
         this(null, place_id, null, null, null, null, null, null);
+    }
+
+    public static FavoriteResponseDto from(Favorite favorite) {
+        return new FavoriteResponseDto(
+                favorite.getPlace().getId(),
+                favorite.getId(),
+                favorite.getUserName(),
+                favorite.getPlace().getPlaceName(),
+                favorite.getPlace().getAddress(),
+                favorite.getPlace().getPhoneNumber(),
+                favorite.getPlace().getCurrentNumberOfPeople(),
+                favorite.getPlace().getCapacity()
+        );
     }
 }
