793038ecc8a661af7b72f8e6ea81cc8803eeff21
Lavine <zihun35@gmail.com>
2022-01-03 16:49:27 +0900

    (BE) MemberController 기능 추가에 따른 dto 수정

--- backend/src/main/java/com/ktds/covidsystem/dto/MemberResponseDto.java
+++ backend/src/main/java/com/ktds/covidsystem/dto/MemberResponseDto.java
@@ -1,9 +1,24 @@
 package com.ktds.covidsystem.dto;
 
+import com.ktds.covidsystem.constant.Authority;
 import com.ktds.covidsystem.domain.Member;
 
-public record MemberResponseDto(String name, String email) {
+public record MemberResponseDto(Long id, String name, String email, Authority authority) {
+
+    public MemberResponseDto(String name, String email) {
+        this(null, name, email, null);
+    }
+
+    public MemberResponseDto(Long id) {
+        this(id, null, null, null);
+    }
+
     public static MemberResponseDto of(Member member) {
         return new MemberResponseDto(member.getName(), member.getEmail());
     }
+
+    public static MemberResponseDto ofWithAuthority(Member member) {
+        return new MemberResponseDto(member.getId(), member.getName(), member.getEmail(), member.getAuthority());
+    }
+
 }
